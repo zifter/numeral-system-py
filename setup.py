570050977b10403241d89b42569671f688d1929b
Oleg Strokachuk <zifter.ai@gmail.com>
2019-12-23 16:43:39 +0300

    Separate requirements, add file for readth

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./src'))
 
 
-setup()
+import numeral_system
+
+setup(
+    name=numeral_system.__full_name__,
+    version=numeral_system.__version__,
+    description=numeral_system.__doc__,
+    author=numeral_system.__author__,
+    author_email=numeral_system.__author_email__,
+)
