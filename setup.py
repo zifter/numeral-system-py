1614e10358c2f37200d0a37a529e4a4d0fef1747
Oleg Strokachuk <zifter.ai@gmail.com>
2020-01-06 14:38:49 +0300

    Fix uploading to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import numeral_system
 setup(
     name=numeral_system.__full_name__,
     version=numeral_system.__version__,
-    description=numeral_system.__doc__,
+    description=numeral_system.__doc__.strip(),  # https://github.com/pypa/twine/issues/522
     author=numeral_system.__author__,
     author_email=numeral_system.__author_email__,
 )
